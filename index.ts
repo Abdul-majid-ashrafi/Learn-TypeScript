68fcd579cec53121980aa896d6274abc9f0b8fde
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-01-13 17:10:46 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,9 @@
+// Without Data Types
+
+let firstName = "Majid";
+let lastName = "Ashraf";
+let phoneNumber = 123456;
+
+console.log("firstName ", firstName)
+console.log("lastName ", lastName)
+console.log("phoneNumber ", phoneNumber)
